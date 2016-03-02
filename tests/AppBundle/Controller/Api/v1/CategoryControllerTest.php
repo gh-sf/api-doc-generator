db60ca74442621aaa075e964e75d37f5db2ace80
Maxim Zhurilo <fumus.27@gmail.com>
2016-03-02 11:56:09 +0200

    tests for categories added

--- tests/AppBundle/Controller/Api/v1/CategoryControllerTest.php
+++ tests/AppBundle/Controller/Api/v1/CategoryControllerTest.php
@@ -2,19 +2,19 @@
 /**
  * Created by PhpStorm.
  * User: fumus
- * Date: 29.02.16
- * Time: 17:52
+ * Date: 02.03.16
+ * Time: 0:12
  */
 
 namespace tests\AppBundle\Controller\Api\v1;
+
 
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
-
-class ProductControllerTest extends WebTestCase
+class CategoryControllerTest extends WebTestCase
 {
 
     //protected $crawler;
@@ -44,7 +44,6 @@ class ProductControllerTest extends WebTestCase
         $application->run($input, new ConsoleOutput());
     }
 
-
     /**
      * @param $expectedCode
      * @param $route
@@ -55,75 +54,51 @@ class ProductControllerTest extends WebTestCase
         $this->requestTest($expectedCode, $route, $method);
     }
 
-    public function testGetProduct()
-    {
-        $this->client->request('GET', '/api/v1/products/1');
-        $json = $this->client->getResponse()->getContent();
-
-        $array = json_decode($json, true);
-        $this->assertProductProperties($array);
-    }
-
-    public function testCgetProduct()
-    {
-        $this->client->request('GET', '/api/v1/products');
-        $content = $this->client->getResponse()->getContent();
-        $array = json_decode($content, true);
-
-        $this->assertTrue(4 == count($array));
-        foreach ($array as $item) {
-
-            $this->assertProductProperties($item);
-        }
-    }
-
-    public function testPutProduct()
-    {
-        $data = array(
-            'name' => 'new_name',
-            'price' => 111,
-        );
-        $this->client->request('PUT', '/api/v1/products/1', $data);
-        $statusCode = $this->client->getResponse()->getStatusCode();
-        $this->assertTrue(204 == $statusCode);
-
-    }
-
-    public function testPostProduct()
-    {
-        $data = array(
-            'name' => 'some_name',
-            'price' => 500,
-            'category' => 'testcategory1',
-            'description' => 'some description',
-        );
-        $this->client->request('POST', '/api/v1/products', $data);
-        $statusCode = $this->client->getResponse()->getStatusCode();
-        $this->assertTrue(204 == $statusCode);
-    }
-
     public function statusCodesProvider()
     {
         return [
-            [200, "/api/v1/products", "GET"],
-            [200, "/api/v1/products/1", "GET"],
-            [200, "/api/v1/products/2", "GET"],
-            [400, "/api/v1/products/100", "GET"],
-            [400, "/api/v1/products/0", "GET"],
-            [405, "/api/v1/products/1", "POST"],
-            [405, "/api/v1/products", "PUT"],
-            [405, "/api/v1/products", "DELETE"],
-            [204, "/api/v1/products/2", "DELETE"],
+            [200, "/api/v1/categories", "GET"],
+            [409, "/api/v1/categories/1", "DELETE"], //can't delete category is not empty
+            [204, "/api/v1/categories/3", "DELETE"], //category is empty
+            [405, "/api/v1/categories", "DELETE"],
+            [405, "/api/v1/categories/1", "POST"],
+            [405, "/api/v1/categories", "PUT"],
         ];
     }
 
-    private function assertProductProperties(array $item)
+    public function testCgetCategory()
     {
-        $this->assertArrayHasKey('id', $item);
-        $this->assertArrayHasKey('name', $item);
-        $this->assertArrayHasKey('category', $item);
-        $this->assertArrayHasKey('price', $item);
-        $this->assertArrayHasKey('description', $item);
+        $this->client->request('GET', '/api/v1/categories');
+        $content = $this->client->getResponse()->getContent();
+        $array = json_decode($content, true);
+
+        $this->assertTrue(3 == count($array));
+        foreach ($array as $item) {
+
+            $this->assertArrayHasKey('name', $item);
+            $this->assertArrayHasKey('id', $item);
+        }
+    }
+
+    public function testPutCategory()
+    {
+        $data = array(
+            'name' => 'new_name',
+        );
+        $this->client->request('PUT', '/api/v1/categories/1', $data);
+        $statusCode = $this->client->getResponse()->getStatusCode();
+        $this->assertTrue(204 == $statusCode);
+
+    }
+
+    public function testPostCategory()
+    {
+        $data = array(
+            'name' => 'some_name',
+        );
+        $this->client->request('POST', '/api/v1/categories', $data);
+        $statusCode = $this->client->getResponse()->getStatusCode();
+        $this->assertTrue(204 == $statusCode);
     }
 
     protected function requestTest($expectedStatusCode, $path, $method = 'GET')
